feb6562b3d73215ae0e2846ebe5251ba9447f0ec
Nicolas <n.mazacerda@hotmail.com>
2023-07-03 20:00:39 -0400

    Add default prop value for list item container

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-
-const ItemListContainer = ({ greeting }) => {
+const ItemListContainer = (
+  { greeting = "Bienvenido Default" } = { greeting: "Bienvenido Default" }
+) => {
   return (
     <div className="d-flex flex-column align-items-center">
       <h1 className="display-4 text-white fw-bolder">{greeting}</h1>
-      <p className="lead fw-normal text-white-50 mb-0">
-        Proximamente...
-      </p>
+      <p className="lead fw-normal text-white-50 mb-0">Proximamente...</p>
     </div>
   );
 };
