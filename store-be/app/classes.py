97add75133095aa2d026f8a795c1ebde8760bc03
Nicolas <n.mazacerda@hotmail.com>
2023-12-14 00:13:08 -0300

    add support for OID on auto documentation

--- store-be/app/classes.py
+++ store-be/app/classes.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from pydantic_core import core_schema
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from typing import Any, List, Optional, Required
 from bson import ObjectId
@@ -16,8 +17,27 @@ class OID(ObjectId):
         return ObjectId(v)
 
     @classmethod
-    def __get_pydantic_json_schema__(cls, field_schema):
-        field_schema.update(type="string")
+    def __get_pydantic_core_schema__(
+        cls, _source_type: Any, _handler: Any
+    ) -> core_schema.CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.union_schema(
+                [
+                    core_schema.is_instance_schema(ObjectId),
+                    core_schema.chain_schema(
+                        [
+                            core_schema.str_schema(),
+                            core_schema.no_info_plain_validator_function(cls.validate),
+                        ]
+                    ),
+                ]
+            ),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda x: str(x)
+            ),
+        )
+
 
 class Product(BaseModel):
     id: OID = Field(default_factory=OID, alias="_id")
@@ -29,9 +49,9 @@ class Product(BaseModel):
     category: OID = Field(default_factory=OID)
     artist: Optional[str] = None
     tracklist: Optional[List[str]] = None
-    
+
     class Config:
-        exclude = ['name']
+        exclude = ["name"]
         populate_by_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
@@ -88,14 +108,21 @@ class CreatedOrder(BaseModel):
 
 
 class MyDB(AsyncIOMotorClient):
-    database = os.getenv("MAIN_DB", 'vshop')
-    connection_string = os.getenv("CONNECTION_STR", 'mongodb://localhost:27017/')
-    tls = bool(os.environ.get('MONGOTLS', False))
-    pem_file = os.environ.get('MONGOPEM', None)
-    auth_mechanism = os.environ.get('MONGOAM', 'MONGODB-X509')
+    database = os.getenv("MAIN_DB", "vshop")
+    connection_string = os.getenv("CONNECTION_STR", "mongodb://localhost:27017/")
+    tls = bool(os.environ.get("MONGOTLS", False))
+    pem_file = os.environ.get("MONGOPEM", None)
+    auth_mechanism = os.environ.get("MONGOAM", "MONGODB-X509")
 
     def __init__(self) -> None:
-        super(MyDB, self).__init__(self.connection_string, tls=self.tls, tlsCertificateKeyFile=self.pem_file, authMechanism=self.auth_mechanism)
-    
-    def __call__(self, collection: str, *args: Any, **kwargs: Any) -> AsyncIOMotorCollection:
+        super(MyDB, self).__init__(
+            self.connection_string,
+            tls=self.tls,
+            tlsCertificateKeyFile=self.pem_file,
+            authMechanism=self.auth_mechanism,
+        )
+
+    def __call__(
+        self, collection: str, *args: Any, **kwargs: Any
+    ) -> AsyncIOMotorCollection:
         return self[self.database][collection]
